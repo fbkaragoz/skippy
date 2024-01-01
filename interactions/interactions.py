dab89ce35c3e0daafe917ba7427c8c6ba4d06f0a
rekurrenzk <Fatihburak@protonmail.com>
2024-01-01 10:13:07 +0300

    Token encryption for sAPI

---
+++ interactions/interactions.py
@@ -0,0 +1,14 @@
+import interactions
+import tokens
+
+discord_api_key = []
+
+for token in tokens:
+    return discord_api_key
+
+
+bot = interactions.Client()
+@interactions.listen()
+async def on_startup():
+    print("Alice is ready..!")
+bot.start(discord_api_key)
